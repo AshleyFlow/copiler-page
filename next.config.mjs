45f339b8c5e1d0fbee85d3e28b97c2d860682245
Flow <highflowey@gmail.com>
2024-05-24 01:13:53 +0330

    added github button

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/copiler-page",
+    images: { unoptimized: true },
     output: "export"
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,9 @@ import { createRef, useCallback, useEffect, useRef, useState } from "react";
 import ReactCodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 
+import Image from "next/image";
+import github_png from "../../public/github.png";
+
 export default function Home() {
   let ref = createRef<HTMLHeadElement>();
   let [loaded, setLoaded] = useState(false);
@@ -16,11 +19,14 @@ export default function Home() {
   let speed = 0
 
   let boost = (speed: number) {
+    print("before:", self.speed)
+
     \\\\
-    -- anything between \\\\ \\\\ will count as luau code
     -- do it in luau because copiler doesn't have math 💀
     self.speed += speed
     \\\\
+
+    print("after:", self.speed)
   }
 }`);
 
@@ -50,18 +56,38 @@ export default function Home() {
   }, [onChange, src]);
 
   return (
-    <main className="flex h-screen overflow-y-hidden">
-      <ReactCodeMirror
-        className="w-1/2 h-screen overflow-y-scroll"
-        value={src}
-        onChange={onChange}
-        extensions={[javascript({})]}
-      ></ReactCodeMirror>
-      {loaded ? (
-        <pre className="w-1/2 overflow-y-scroll">
-          <code ref={ref} className="text-sm h-screen"></code>
-        </pre>
-      ) : undefined}
+    <main className="h-screen overflow-y-hidden">
+      <div className="flex bg-slate-200 justify-end items-center p-1 box-content">
+        <a
+          href="https://github.com/highflowey/copiler"
+          target="_blank"
+          className="flex items-end justify-center rounded-md p-1 box-content bg-slate-300"
+          style={{ height: "30px" }}
+        >
+          <Image src={github_png} width={30} height={30} alt="Github Mark" />
+          <h1 className="text-1xl font-mono">Github</h1>
+        </a>
+      </div>
+      <div
+        className="flex overflow-y-hidden"
+        style={{ height: "calc(100vh - 30px)" }}
+      >
+        <ReactCodeMirror
+          className="w-1/2 overflow-y-scroll"
+          value={src}
+          onChange={onChange}
+          extensions={[javascript({})]}
+        ></ReactCodeMirror>
+        {loaded ? (
+          <pre className="w-1/2 overflow-y-scroll">
+            <code
+              ref={ref}
+              className="text-sm"
+              style={{ height: "calc(100vh - 30px)" }}
+            ></code>
+          </pre>
+        ) : undefined}
+      </div>
     </main>
   );
 }
